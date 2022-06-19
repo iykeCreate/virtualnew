7663c23eef3a2d156c47785539b829b11826603e
iyke <iykecreat@gmail.com>
2022-06-19 10:02:49 +0100

    second commit

--- iykecreate/blog/admin.py
+++ iykecreate/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import Post
+
 
 # Register your models here.
+class PostDB(admin.ModelAdmin):
+    list_display=[
+         "Title","Text"," Author","Create_date","Publisher_date"
+    ]
--- iykecreate/blog/models.py
+++ iykecreate/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    class Meta:
+
+         verbose_name="Post"
+         verbose_name_plural="Post"
+    Title=models.CharField(max_length=200)
+    Text=models.TextField(max_length=200)
+    Author=models.TextField(max_length=200)
+    Create_date=models.DateTimeField(max_length=200)
+    Publisher_date=models.DateTimeField(max_length=200)
+
